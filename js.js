668d66135d80ce24cd17547a21bc01275548dd93
newbiejas <jaskarannagra@homail.com>
2022-07-22 23:13:26 -0700

    Add dataset attribute to each game square to link them easily with the array which will store player markers and check win conditions. Place player generator method inside gameObj as a private function. Create start game method and add it to gameObj which sets player turn and sets variable value stating which players turn it is.

--- js.js
+++ js.js
@@ -1,20 +1,37 @@
-const Gameboard = (function(){
-    let gameboard = ['x' ,'x' , 'x', 'o', 'o', 'o' , 'x', 'x', 'x' ];
+const gameObj = (function () {
+  let _gameboard = ["x", "x", "x", "o", "o", "o", "x", "x", "x"];
 
-    return {
-        gameboard
+  (function render() {
+    for (let i = 0; i < 9; i++) {
+      let box = document.getElementById(`a${i}`);
+      box.innerText = _gameboard[i];
+      box.dataset.square = i
     }
-})()
+  })();
 
-const playerGenerator = name => {
-    return {
-        name
+  const playerGenerator = (name, marker) => {
+    return{
+        name,
+        marker
     }
-}
+};
 
-(function render(){
-    for (let i=0;i < 9; i++){
-        let box = document.getElementById(`a${i}`);
-        box.innerText = Gameboard.gameboard[i]
-    }
-})()
+  const startGame = (name, marker) => {
+    let player = playerGenerator(name, marker);
+    let turn = "player";
+    const playarea = document.getElementById("playarea");
+    playarea.addEventListener("click", function (e) {
+      if (turn == "player") {
+        e.target.innerText = player.marker;
+
+        _gameboard[`${e.target.dataset.square}`] = player.marker
+
+        turn = "cpu";
+
+      }
+    });
+  };
+  return {
+    startGame,
+  };
+})();
