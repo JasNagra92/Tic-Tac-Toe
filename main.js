633bb5b6cead512139bd8f359dc6b705af9811a8
newbiejas <jaskarannagra@homail.com>
2022-07-23 19:38:57 -0700

    add checkWin function that checks win conditions after every marker placement

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const gameObj = (function () {
-  let _gameboard = ["x", "x", "x", "o", "o", "o", "x", "x", "x"];
+  let _gameboard = ["", "", "", "", "", "", "", "", ""];
   let turn = "player1";
   (function render() {
     for (let i = 0; i < 9; i++) {
@@ -15,24 +15,47 @@ const gameObj = (function () {
       marker,
     };
   };
+  document.getElementById("startGame").addEventListener("click", function () {
+    const player1name = document.getElementById("player1name").value;
+    const player2name = document.getElementById("player2name").value;
+    const player1marker = document.querySelector(
+      `input[name="marker1"]:checked`
+    ).value;
+    const player2marker = document.querySelector(
+      `input[name="marker2"]:checked`
+    ).value;
+    if (player1name == "") {
+      alert("player 1 name cannot be empty");
+    } else if (player2name == "") {
+      alert("player 2 name cannot be empty");
+    } else if (player1marker == player2marker) {
+      alert("players cannot have same marker");
+    } else {
+      startGame(player1name, player1marker, player2name, player2marker);
+    }
+  });
 
-    document
-    .getElementById("startGame")
-    .addEventListener("click", function () {
-      const player1name = document.getElementById("player1name").value;
-      const player2name = document.getElementById("player2name").value;
-      const player1marker = document.querySelector(
-        `input[name="marker1"]:checked`
-      ).value;
-      const player2marker = document.querySelector(
-        `input[name="marker2"]:checked`
-      ).value;
-      if (player1name == ""){alert('player 1 name cannot be empty')}
-      else if (player2name == ""){alert('player 2 name cannot be empty')}
-      else if (player1marker == player2marker){alert('players cannot have same marker')}
-      else{
-      startGame(player1name, player1marker, player2name, player2marker)};
-    });
+  checkWin = function(player1name,player2name){
+    if (_gameboard[0] == "X" && _gameboard[1] == "X" && _gameboard[2] == 'X' ||
+        _gameboard[3] == "X" && _gameboard[4] == "X" && _gameboard[5] == 'X' ||
+        _gameboard[6] == "X" && _gameboard[7] == "X" && _gameboard[8] == 'X' ||
+        _gameboard[0] == "X" && _gameboard[3] == "X" && _gameboard[6] == 'X' ||
+        _gameboard[1] == "X" && _gameboard[4] == "X" && _gameboard[7] == 'X' ||
+        _gameboard[2] == "X" && _gameboard[5] == "X" && _gameboard[8] == 'X' ||
+        _gameboard[0] == "X" && _gameboard[4] == "X" && _gameboard[8] == 'X' ||
+        _gameboard[2] == "X" && _gameboard[4] == "X" && _gameboard[6] == 'X' ){
+      alert(`${player1name} you win`)
+    } else if (
+        _gameboard[0] == "O" && _gameboard[1] == "O" && _gameboard[2] == 'O' ||
+        _gameboard[3] == "O" && _gameboard[4] == "O" && _gameboard[5] == 'O' ||
+        _gameboard[6] == "O" && _gameboard[7] == "O" && _gameboard[8] == 'O' ||
+        _gameboard[0] == "O" && _gameboard[3] == "O" && _gameboard[6] == 'O' ||
+        _gameboard[1] == "O" && _gameboard[4] == "O" && _gameboard[7] == 'O' ||
+        _gameboard[2] == "O" && _gameboard[5] == "O" && _gameboard[8] == 'O' ||
+        _gameboard[0] == "O" && _gameboard[4] == "O" && _gameboard[8] == 'O' ||
+        _gameboard[2] == "O" && _gameboard[4] == "O" && _gameboard[6] == 'O' ){
+      alert (`${player2name} you win`)
+  }}
 
   const startGame = (
     player1name,
@@ -48,14 +71,16 @@ const gameObj = (function () {
         e.target.innerText = player1.marker;
         _gameboard[`${e.target.dataset.square}`] = player1.marker;
         turn = "player2";
+        checkWin(player1.name,player2.name);
       } else if (turn == "player2") {
         e.target.innerText = player2.marker;
         _gameboard[`${e.target.dataset.square}`] = player2.marker;
         turn = "player1";
+        checkWin();
       }
     });
   };
   return {
-    startGame,
-  };
+    checkWin
+  }
 })();
