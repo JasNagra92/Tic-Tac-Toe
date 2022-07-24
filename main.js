ac7f157276e86fd520a73ba0452af975be24214f
newbiejas <jaskarannagra@homail.com>
2022-07-23 19:47:43 -0700

    formatting

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const gameObj = (function () {
   let _gameboard = ["", "", "", "", "", "", "", "", ""];
   let turn = "player1";
+
   (function render() {
     for (let i = 0; i < 9; i++) {
       let box = document.getElementById(`a${i}`);
@@ -15,6 +16,7 @@ const gameObj = (function () {
       marker,
     };
   };
+
   document.getElementById("startGame").addEventListener("click", function () {
     const player1name = document.getElementById("player1name").value;
     const player2name = document.getElementById("player2name").value;
@@ -35,27 +37,31 @@ const gameObj = (function () {
     }
   });
 
-  checkWin = function(player1name,player2name){
-    if (_gameboard[0] == "X" && _gameboard[1] == "X" && _gameboard[2] == 'X' ||
-        _gameboard[3] == "X" && _gameboard[4] == "X" && _gameboard[5] == 'X' ||
-        _gameboard[6] == "X" && _gameboard[7] == "X" && _gameboard[8] == 'X' ||
-        _gameboard[0] == "X" && _gameboard[3] == "X" && _gameboard[6] == 'X' ||
-        _gameboard[1] == "X" && _gameboard[4] == "X" && _gameboard[7] == 'X' ||
-        _gameboard[2] == "X" && _gameboard[5] == "X" && _gameboard[8] == 'X' ||
-        _gameboard[0] == "X" && _gameboard[4] == "X" && _gameboard[8] == 'X' ||
-        _gameboard[2] == "X" && _gameboard[4] == "X" && _gameboard[6] == 'X' ){
-      alert(`${player1name} you win`)
+  checkWin = function (player1name, player2name) {
+    if (
+      (_gameboard[0] == "X" && _gameboard[1] == "X" && _gameboard[2] == "X") ||
+      (_gameboard[3] == "X" && _gameboard[4] == "X" && _gameboard[5] == "X") ||
+      (_gameboard[6] == "X" && _gameboard[7] == "X" && _gameboard[8] == "X") ||
+      (_gameboard[0] == "X" && _gameboard[3] == "X" && _gameboard[6] == "X") ||
+      (_gameboard[1] == "X" && _gameboard[4] == "X" && _gameboard[7] == "X") ||
+      (_gameboard[2] == "X" && _gameboard[5] == "X" && _gameboard[8] == "X") ||
+      (_gameboard[0] == "X" && _gameboard[4] == "X" && _gameboard[8] == "X") ||
+      (_gameboard[2] == "X" && _gameboard[4] == "X" && _gameboard[6] == "X")
+    ) {
+      alert(`${player1name} you win`);
     } else if (
-        _gameboard[0] == "O" && _gameboard[1] == "O" && _gameboard[2] == 'O' ||
-        _gameboard[3] == "O" && _gameboard[4] == "O" && _gameboard[5] == 'O' ||
-        _gameboard[6] == "O" && _gameboard[7] == "O" && _gameboard[8] == 'O' ||
-        _gameboard[0] == "O" && _gameboard[3] == "O" && _gameboard[6] == 'O' ||
-        _gameboard[1] == "O" && _gameboard[4] == "O" && _gameboard[7] == 'O' ||
-        _gameboard[2] == "O" && _gameboard[5] == "O" && _gameboard[8] == 'O' ||
-        _gameboard[0] == "O" && _gameboard[4] == "O" && _gameboard[8] == 'O' ||
-        _gameboard[2] == "O" && _gameboard[4] == "O" && _gameboard[6] == 'O' ){
-      alert (`${player2name} you win`)
-  }}
+      (_gameboard[0] == "O" && _gameboard[1] == "O" && _gameboard[2] == "O") ||
+      (_gameboard[3] == "O" && _gameboard[4] == "O" && _gameboard[5] == "O") ||
+      (_gameboard[6] == "O" && _gameboard[7] == "O" && _gameboard[8] == "O") ||
+      (_gameboard[0] == "O" && _gameboard[3] == "O" && _gameboard[6] == "O") ||
+      (_gameboard[1] == "O" && _gameboard[4] == "O" && _gameboard[7] == "O") ||
+      (_gameboard[2] == "O" && _gameboard[5] == "O" && _gameboard[8] == "O") ||
+      (_gameboard[0] == "O" && _gameboard[4] == "O" && _gameboard[8] == "O") ||
+      (_gameboard[2] == "O" && _gameboard[4] == "O" && _gameboard[6] == "O")
+    ) {
+      alert(`${player2name} you win`);
+    }
+  };
 
   const startGame = (
     player1name,
@@ -67,24 +73,24 @@ const gameObj = (function () {
     let player2 = playerGenerator(player2name, player2marker);
     const playarea = document.getElementById("playarea");
     playarea.addEventListener("click", function (e) {
-      if (turn == "player1" && e.target.innerText == player2.marker){
-        alert('must pick empty square');
-        turn = "player1"
-      } else if(turn == 'player1' && e.target.innerText == player1.marker){
-        alert('You already place a marker here');
-        turn = "player1"
-      } else if(turn == 'player1'){
+      if (turn == "player1" && e.target.innerText == player2.marker) {
+        alert("must pick empty square");
+        turn = "player1";
+      } else if (turn == "player1" && e.target.innerText == player1.marker) {
+        alert("You already place a marker here");
+        turn = "player1";
+      } else if (turn == "player1") {
         e.target.innerText = player1.marker;
         _gameboard[`${e.target.dataset.square}`] = player1.marker;
         turn = "player2";
-        checkWin(player1.name,player2.name);
-      } else if (turn == "player2" && e.target.innerText == player2.marker){
-        alert('You already place a marker here');
+        checkWin(player1.name, player2.name);
+      } else if (turn == "player2" && e.target.innerText == player2.marker) {
+        alert("You already place a marker here");
         turn = "player2";
-      } else if (turn == 'player2' && e.target.innerText == player1.marker){
-        alert('must pick empty square');
-        turn = "player2"
-      } else if (turn == 'player2'){
+      } else if (turn == "player2" && e.target.innerText == player1.marker) {
+        alert("must pick empty square");
+        turn = "player2";
+      } else if (turn == "player2") {
         e.target.innerText = player2.marker;
         _gameboard[`${e.target.dataset.square}`] = player2.marker;
         turn = "player1";
@@ -92,7 +98,4 @@ const gameObj = (function () {
       }
     });
   };
-  return {
-    checkWin
-  }
 })();
